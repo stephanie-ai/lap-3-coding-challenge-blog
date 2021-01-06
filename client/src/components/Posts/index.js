58f7f9acecfedf07f90ba455c457aabc88ca3c0e
stephanie-ai <stephanie_cooper@hotmail.com>
2021-01-06 11:33:20 +0000

    add delete button functionality

--- client/src/components/Posts/index.js
+++ client/src/components/Posts/index.js
@@ -8,11 +8,25 @@ class Posts extends React.Component{
     }
 
     componentDidMount(){
+        this.getData();
+    }
+
+    getData(){
         fetch('http://localhost:3000/posts')
             .then(r => r.json())
             .then(data => {
                 this.setState({allposts: data.posts})
             })
+    }
+
+    deletePost(id){
+        const options = {
+            method: 'DELETE'
+        };
+        
+        fetch(`http://localhost:3000/posts/${id}`, options)
+            .then(this.getData())
+            .catch(console.warn())
     }
 
     render() {
@@ -21,6 +35,7 @@ class Posts extends React.Component{
                 <p><b>Title:</b> {post.title}</p>
                 <p><b>Name:</b> {post.name}</p>
                 <p><b>Story:</b> {post.story}</p>
+                <button onClick={()=> this.deletePost(post.id)}>Delete</button>
             </div>
         ))
 

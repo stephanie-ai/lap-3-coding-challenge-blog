b3f74e0111215bd603bfc8250b764793216bed48
stephanie-ai <stephanie_cooper@hotmail.com>
2021-01-08 13:50:34 +0000

    check routes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -27,7 +27,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const post = await Post.create(req.body.title, req.body.name, req.body.story);
-        res.json(post);
+        await res.json(post);
     } catch (err) {
         res.status(404).json({err});
     }
